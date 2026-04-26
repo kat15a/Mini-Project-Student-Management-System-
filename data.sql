d96cb9f6af49dd180a648772aa5bc752a46d787a
Aadrika Katiyar <katiyaraadrika15@gmail.com>
2026-04-26 20:52:27 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+CALL addstudents(4, 'Rajveer', 'M', '+9193278235', 'rajveer@gmail.com');
+
+INSERT INTO subjects(subject_id, suject_name)
+VALUES (1,'Hindi'), (2,'Math'), (3,'English');
+
+INSERT INTO marks(marks_id, student_id, subject_id, marks)
+VALUES (4,4,1,80),
+       (5,4,2,85),
+       (6,4,3,90);
